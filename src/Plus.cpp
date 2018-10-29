4288f5bbfa919faf658a81d53bb149ec8408ae70
chlehd0509 <chlehd0509@gmail.com>
2018-10-29 09:01:21 -0700

    xor

--- src/Plus.cpp
+++ src/Plus.cpp
@@ -11,8 +11,8 @@ int main(){
 	const char graph_path[20] = "Plus_Model.tflite\0";
 	const int num_threads = 1;
 	std::string input_layer_type = "float";
-	std::vector<int> sizes = {3};
-	float x,y,z;
+	std::vector<int> sizes = {8};
+	float x,y,z,x2,y2,z2,x3,y3;
 
 	std::unique_ptr<tflite::FlatBufferModel> model(
 		tflite::FlatBufferModel::BuildFromFile(graph_path));
@@ -45,11 +45,16 @@ int main(){
 	}
 
 	//read two numbers
-	std::printf("Type three float numbers : ");
-	std::scanf("%f %f %f", &x, &y, &z);
+	std::printf("Type eight float numbers : ");
+	std::scanf("%f %f %f", &x, &y, &z, &x2, &y2, &z2, &x3, &y3);
 	interpreter->typed_input_tensor<float>(0)[0] = x;
 	interpreter->typed_input_tensor<float>(0)[1] = y;
 	interpreter->typed_input_tensor<float>(0)[2] = z;
+	interpreter->typed_input_tensor<float>(0)[3] = x2;
+	interpreter->typed_input_tensor<float>(0)[4] = y2;
+	interpreter->typed_input_tensor<float>(0)[5] = z2;
+	interpreter->typed_input_tensor<float>(0)[6] = x3;
+	interpreter->typed_input_tensor<float>(0)[7] = y3;
 
 	if(interpreter->Invoke() != kTfLiteOk){
 		std::printf("Failed to invoke!\n");
