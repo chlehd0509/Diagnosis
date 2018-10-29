7e4f27a4d42cae117f4106f7507877f0e2be4064
chlehd0509 <chlehd0509@gmail.com>
2018-10-29 09:11:36 -0700

    xor

--- src/Plus.cpp
+++ src/Plus.cpp
@@ -14,6 +14,98 @@ int main(){
 	std::vector<int> sizes = {8};
 	float x,y,z,x2,y2,z2,x3,y3;
 
+
+
+
+
+        if (argc != 2){
+            printf ("input : [exe] [log file]\n");
+            exit(0); 
+        }
+
+        unsigned int conj_bit = 16;
+        unsigned int wordlist_size = 128;
+
+	const char *graph_path = argv[1];
+        const char *wordfile = argv[2];
+        const char *logfile = argv[3];
+        size_t size;
+        std::vector<float> bits_val;
+
+        std::vector<std::string> word_arr;
+
+        printf ("Read word file...\n");
+        std::ifstream t1(wordfile);
+        if (!t1) {
+                printf ("There's no word file\n");
+                exit(0);
+        }
+        t1.seekg(0, std::ios::end);
+        size = t1.tellg();
+        std::string wordstr(size, ' ');
+        t1.seekg(0);
+        t1.read(&wordstr[0], size); 
+
+        printf ("Read log file...\n");
+        std::ifstream t2(logfile);
+        if (!t2) {
+                printf ("There's no log file\n");
+                exit(0);
+        }
+        t2.seekg(0, std::ios::end);
+        size = t2.tellg();
+        std::string logstr(size, ' ');
+        t2.seekg(0);
+        t2.read(&logstr[0], size); 
+
+        printf ("Make Wordlist...\n");
+        unsigned int len = wordstr.length();
+        unsigned int bef_place = 0;
+        for (unsigned int i = 0;i < len;i++) {
+                if (wordstr[i] == '\n') { 
+                        wordstr[i] = 0; 
+                        word_arr.push_back(&wordstr[bef_place]);
+                        bef_place = i+1;
+                }
+        }
+
+        if (word_arr.size() != wordlist_size) {
+                printf ("Word Count is not 128\n");
+                exit(0);
+        }
+
+
+        for (unsigned int i = 0;i < logstr.length();i++) {
+                if (logstr[i] == '\n') { logstr[i] = ' '; }
+                else if ((logstr[i] >= 'A') && (logstr[i] <= 'Z')) {
+                        logstr[i] += ('a' - 'A');
+                }
+        }
+
+
+
+        for (int i = 0;i < wordlist_size;i++) {
+
+                if ((i%conj_bit == 0) && (i != 0)) {
+                    bits_val.push_back(bit_coll);
+                    bit_coll = 0.0f;
+                }
+
+                if (logstr.find(word_arr[i]) != std::string::npos) { 
+                    bit_coll = (bit_coll * 2.0f) + 1.0f;
+                }
+                else { 
+                    bit_coll = (bit_coll * 2.0f);
+                }
+
+        }
+
+
+
+
+
+
+
 	std::unique_ptr<tflite::FlatBufferModel> model(
 		tflite::FlatBufferModel::BuildFromFile(graph_path));
 
@@ -45,8 +137,9 @@ int main(){
 	}
 
 	//read two numbers
+/*
 	std::printf("Type eight float numbers : ");
-	std::scanf("%f %f %f", &x, &y, &z, &x2, &y2, &z2, &x3, &y3);
+	std::scanf("%f %f %f %f %f %f %f %f", &x, &y, &z, &x2, &y2, &z2, &x3, &y3);
 	interpreter->typed_input_tensor<float>(0)[0] = x;
 	interpreter->typed_input_tensor<float>(0)[1] = y;
 	interpreter->typed_input_tensor<float>(0)[2] = z;
@@ -55,6 +148,10 @@ int main(){
 	interpreter->typed_input_tensor<float>(0)[5] = z2;
 	interpreter->typed_input_tensor<float>(0)[6] = x3;
 	interpreter->typed_input_tensor<float>(0)[7] = y3;
+*/
+        for (int i = 0;i < 8;i++) {
+                interpreter->typed_input_tensor<float>(0)[i] = bits_val[i];
+        }
 
 	if(interpreter->Invoke() != kTfLiteOk){
 		std::printf("Failed to invoke!\n");
